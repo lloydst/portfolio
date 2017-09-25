84402347d87855688c6a298cbf85228972bda0ca
lloydst <lloyd.stumpel@gmail.com>
2017-09-25 15:43:13 +0200

    minor

--- client/app/blog/blog.component.ts
+++ client/app/blog/blog.component.ts
@@ -13,3 +13,4 @@ export class BlogComponent implements OnInit {
   }
 
 }
+
---
+++ client/app/blog/blog.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+@Injectable()
+export class BlogService {
+     result: any;
+     constructor(private _http: Http) { }
+     // using these to access the db
+     getBlog() {
+          // gets the json on this end point. host: /api/blog
+       return this._http.get('/api/blog')
+         .map(result => this.result = result.json().data);
+     }
+     postBlog() {
+          // code here to clean up text area's and post to the api/blog endpoint
+          return "I'm not implemented yet";
+     }
+}
+
