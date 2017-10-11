7b90524e1c58027516bbfead4f2af36d3117ee84
lloydst <lloyd.stumpel@gmail.com>
2017-10-11 16:14:55 +0200

    m

--- test/test_suite.js
+++ test/test_suite.js
@@ -1,18 +1,25 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const fs = require('fs');
+const webdriver = require('selenium-webdriver');
+const chromedriver = require('chromedriver');
 
-let driverFf = new Builder()
-    .forBrowser('firefox')
-    .build();
-let driverCh = new Builder()
-     .forBrowser('chrome')
-     .build();
+const chromeCapabilities = webdriver.Capabilities.chrome();
+chromeCapabilities.set('chromeOptions', {args: ['headless','disable-gpu','window-size=1920x1080']});
 
-Test(driverCh);
-//Test(driverFf);
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .withCapabilities(chromeCapabilities)
+  .build();
 
+// Navigate to google.com, enter a search.
 function Test(driver) {
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-driver.wait(until.titleIs('webdriver - Google Search'), 10000);
-driver.quit();
+     driver.get('http://www.google.com/ncr');
+     driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+     driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+
+// Take screenshot of results page. Save to disk.
+driver.takeScreenshot().then(base64png => {
+  fs.writeFileSync('screenshot.png', new Buffer(base64png, 'base64'));
+});
 }
+
+driver.quit();
